b3efb03f86304d7223d7c53a6011ff198d146ffc
muhammad razi <muhammadrazipmdc@gmail.com>
2022-03-09 11:45:37 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [value , setValue] = useState(null)
+  const [numbersArr, setNumbersArr] = useState([])
+  console.log("::numbersArr...",numbersArr);
+
+  const createArray = () => {
+    const numbersArray = [];
+    const length =  value * value;
+    Array.apply(null, { length: length }).map((num , index) => {
+      numbersArray.push(parseInt(index) + 1)
+    }
+    )
+    setNumbersArr(numbersArray.sort(function(){ return Math.random() - 0.5}))
+  }
+
+  const handleChange = () =>{
+    createArray()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <p>Alpha Task</p>
+     <div>
+       <input
+       type='number'
+       placeholder='Please Enter Number'
+       value={value}
+       onChange={e =>setValue(e.target.value)}
+       />
+       <button onClick={handleChange}>Submit</button>
+       {
+         value ? (
+           <div style={{height: '30px', width: '30px', display:'flex', backgroundColor: 'gray'}}>
+             {
+               Array.apply(null, { length: value }).map((r) => (
+                 <>
+                  <div key={r}>
+                    {
+                      Array.apply(null, { length: value }).map((c) => (
+                        <div key={c} style={{height: '30px', width: '30px', backgroundColor: 'gray'}}>
+                          {numbersArr}
+                        </div>
+                      ))
+                    }
+                  </div>
+                  <br />
+                </>
+              ))
+             }
+           </div>
+         ): null
+       }
+     </div>
     </div>
   );
 }
